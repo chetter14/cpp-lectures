a3d866a4cd48040c225d36c271844312f2031e48
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-02-11 01:52:39 +0300

    Complete the sample in runtime polymorphism part

--- sean-parent-runtime-polymorph/main.cpp
+++ sean-parent-runtime-polymorph/main.cpp
@@ -9,15 +9,11 @@ namespace {
         MyValue(double x, double y) : x_(x), y_(y) { 
             std::cout << "Constructor" << std::endl; 
         }
-        MyValue(const MyValue& val) {
+        MyValue(const MyValue& val) : x_(val.x_), y_(val.y_) {
             std::cout << "Copy constructor" << std::endl;
-            x_ = val.x_;
-            y_ = val.y_;
         }
-        MyValue(MyValue&& val) {
+        MyValue(MyValue&& val) : x_(val.x_), y_(val.y_) {
             std::cout << "Move constructor" << std::endl;
-            x_ = val.x_;
-            y_ = val.y_;
             val.x_ = 0.0;
             val.y_ = 0.0;
         }
@@ -52,29 +48,42 @@ void draw<MyValue>(const MyValue& val, std::ostream& os) {
 }
 
 int main() {
-    Doc::Document mydoc;
+    {
+        Doc::Document mydoc;
 
-    // mydoc.emplace_back(1);
-    // mydoc.emplace_back(3.14);
-    // mydoc.emplace_back(std::string("sample text"));
+        mydoc.emplace_back(1);
+        mydoc.emplace_back(3.14);
+        mydoc.emplace_back(std::string("sample text"));
 
-    // int a = 2;
-    // double b = 9.81;
-    // std::string c = "text #2";
+        int a = 2;
+        double b = 9.81;
+        std::string c = "to be moved from";
+        std::string d = "stays the same";
 
-    // mydoc.emplace_back(a);
-    // mydoc.emplace_back(b);
-    // mydoc.emplace_back(std::move(c));
+        mydoc.emplace_back(a);
+        mydoc.emplace_back(b);
+        mydoc.emplace_back(std::move(c));
+        mydoc.emplace_back(d);
 
-    // Doc::draw(mydoc, std::cout);
+        draw(mydoc, std::cout);
+
+        assert(c.compare("to be moved from") != 0);
+        assert(d.compare("stays the same") == 0);
+    }
 
     {
+        Doc::Document mydoc;
+
         MyValue a(7.3, 15.8);
         mydoc.emplace_back(a);
+
+        MyValue b(11.1, 22.2);
+        mydoc.emplace_back(std::move(b));
 
         draw(mydoc, std::cout);
 
         assert(a.getX() == 7.3 && a.getY() == 15.8);
+        assert(b.getX() == 0.0 && b.getY() == 0.0);
     }
 
     return 0;
