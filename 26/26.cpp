f833de180476e4a62378f07921e6939a1e2a71b2
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-03-07 11:31:52 +0300

    Complete with metaprogramming, begin to deal with constexpr

--- 26/26.cpp
+++ 26/26.cpp
@@ -56,6 +56,41 @@ namespace {
 
     template<int N, int S>
     struct IntSqrt<N, S, S, S> : std::integral_constant<int, S> {};
+
+    template<typename T> struct my_numeric_limits;
+
+    template<> struct my_numeric_limits<char> { 
+        static constexpr std::size_t max() { return 256; }
+    };
+
+    /* With SFINAE: */
+    // template<typename T, std::enable_if_t<(sizeof(T) > 4), int> = 0>
+    // void foo(T x) { 
+    //     std::cout << "sizeof is larger than 4" << std::endl;
+    // }
+
+    // template<typename T, std::enable_if_t<(sizeof(T) <= 4), int> = 0>
+    // void foo(T x) { 
+    //     std::cout << "sizeof is equal or less than 4" << std::endl;
+    // }
+
+    /* Without SFINAE (but with if constexpr): */
+    template<typename T>
+    void foo(T x) {
+        if constexpr (sizeof(T) > 4) {
+            std::cout << "sizeof is larger than 4" << std::endl;
+        } else {
+            std::cout << "sizeof is equal or less than 4" << std::endl;
+        }
+    }
+
+    template<typename Head, typename... Tail>
+    void print(Head head, Tail... tail) {
+        std::cout << head << " ";
+        if constexpr (sizeof...(tail) > 0) {
+            print(tail...);
+        }
+    }
 }
 
 int main() {
@@ -97,6 +132,30 @@ int main() {
         std::cout << IntSqrt<14>::value << std::endl;
         std::cout << IntSqrt<36>::value << std::endl;
         std::cout << IntSqrt<30>::value << std::endl;
+    }
+    print_delim();
+    {
+        constexpr std::size_t sz = my_numeric_limits<char>::max();
+        int arr[sz] = { 1, 2, 3 };
+        std::cout << arr[0] << " " << arr[1] << " " << arr[2] << std::endl;
+    }
+    print_delim();
+    {
+        static constexpr int arr[] = { 5, 2, 8, 0, 1, 3 };
+        constexpr const int *x = &arr[0];
+        std::cout << x << " " << *x << std::endl;
+    }
+    print_delim();
+    {
+        double a = 2.3;
+        short b = 10;
+        foo(a);
+        foo(b);
+    }
+    print_delim();
+    {
+        print(2, 3.14f, 9.81, "string", 'c');
+        std::cout << std::endl;
     }
     print_delim();
     std::cout << "Passed!" << std::endl;
